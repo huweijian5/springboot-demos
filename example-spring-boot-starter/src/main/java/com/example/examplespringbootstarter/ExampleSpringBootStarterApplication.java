851617ca04abc48c5c75b66568956159aadebe4d
huweijian5 <304797017@qq.com>
2018-10-19 20:18:14 +0800

    update

---
+++ example-spring-boot-starter/src/main/java/com/example/examplespringbootstarter/ExampleSpringBootStarterApplication.java
@@ -0,0 +1,12 @@
+package com.example.examplespringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class ExampleSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringBootStarterApplication.class, args);
+	}
+}
